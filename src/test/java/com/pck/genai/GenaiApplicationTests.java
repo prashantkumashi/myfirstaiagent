1be4bc09dd8e3e41811d403c842829a003cdc572
prashantkumashi <55656842+prashantkumashi@users.noreply.github.com>
2025-03-20 18:55:51 +0530

    Update GenaiApplicationTests.java

--- src/test/java/com/pck/genai/GenaiApplicationTests.java
+++ src/test/java/com/pck/genai/GenaiApplicationTests.java
@@ -2,11 +2,8 @@ package com.pck.genai;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.web.server.LocalServerPort;
 
-import org.springframework.web.client.RestTemplate;
-
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT, properties = "server.port=8080")
+@SpringBootTest
 class GenaiApplicationTests {
 
 	@LocalServerPort
