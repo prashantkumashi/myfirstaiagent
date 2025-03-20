34742982496975db5f3f373df32965f2206e5106
prashantkumashi <55656842+prashantkumashi@users.noreply.github.com>
2025-03-20 17:46:47 +0530

    Update GenaiApplicationTests.java

--- src/test/java/com/pck/genai/GenaiApplicationTests.java
+++ src/test/java/com/pck/genai/GenaiApplicationTests.java
@@ -2,7 +2,7 @@ package com.pck.genai;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.boot.test.web.server.LocalServerPort;
 
 import org.springframework.web.client.RestTemplate;
 
