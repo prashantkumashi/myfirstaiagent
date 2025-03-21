29360651b8ed149bb94dffc4f31896b515d3c696
prashantkumashi <55656842+prashantkumashi@users.noreply.github.com>
2025-03-21 10:28:18 +0530

    Renamed class

--- src/main/java/com/pck/genai/config/AppConfig.java
+++ src/main/java/com/pck/genai/config/AppConfig.java
@@ -4,7 +4,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
-public class OpenAIConfig {
+public class AppConfig {
 
     @Value("${openai.api.key}")
     private String openApiKey;
